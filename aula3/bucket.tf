45bf88b7ac99c1a1729a8404937966c2d1d95ca8
Rafael Cysneiros <cys.rafael@gmail.com>
2024-05-18 21:53:00 -0300

    feature: aula 03 provisioners and states - 02

--- aula3/bucket.tf
+++ aula3/bucket.tf
@@ -5,3 +5,24 @@ resource "aws_s3_bucket" "bucket_aula3" {
     Nome = "Meu Bucket Terraform"
   }
 }
+
+resource "aws_s3_bucket_policy" "bucket_aula3_policy" {
+  bucket = aws_s3_bucket.bucket_aula3.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.bucket_aula3.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
